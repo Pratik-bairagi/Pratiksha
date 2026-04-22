34f47f1f83781c2a12f7162998300771a1bbfd28
Pratik-bairagi <49588356+Pratik-bairagi@users.noreply.github.com>
2026-04-22 14:42:45 +0530

    Update Style.js

--- js/Style.js
+++ js/Style.js
@@ -2,23 +2,11 @@ let cart = [];
 
 function addToCart(product) {
   cart.push(product);
-  document.getElementById("cart-count").innerText = cart.length;
   alert(product + " added to cart");
-}
-
-function placeOrder(e) {
-  e.preventDefault();
-
-  let name = document.getElementById("name").value;
-
-  alert("Thank you " + name + " 🌿 Your order placed!");
-  cart = [];
-  document.getElementById("cart-count").innerText = 0;
 }
 
 function sendWhatsApp() {
   let phone = "91XXXXXXXXXX";
   let msg = "I want to order: " + cart.join(", ");
-
   window.open(`https://wa.me/${phone}?text=${encodeURIComponent(msg)}`);
 }
